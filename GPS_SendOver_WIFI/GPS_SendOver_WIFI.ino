faa53de7be580c27b4d88adec207969277865051
Nikolaj Juel Andersen <njan20@student.aau.dk>
2023-10-19 15:52:09 +0200

    Added 2 .ino files, one prints the gps values, the other send the gps values over wifi

---
+++ GPS_SendOver_WIFI/GPS_SendOver_WIFI.ino
@@ -0,0 +1,107 @@
+/*
+   This ESP32 code is created by esp32io.com
+
+   This ESP32 code is released in the public domain
+
+   For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-gps
+*/
+
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <WiFi.h>
+
+#define GPS_BAUDRATE 9600  // The default baudrate of NEO-6M is 9600
+#define RX2 19
+#define TX2 18
+#define PCK_INTERVAL 200
+
+const char* ssid = "followDrone";    // Same as the access point SSID
+const char* password = "AndersOst";  // Same as the access point password
+
+const char* serverIP = "192.168.1.1";  // IP address of the receiver ESP32
+const int serverPort = 80;
+
+
+WiFiClient client;
+
+TinyGPSPlus gps;  // the TinyGPS++ object
+EspSoftwareSerial::UART ss;
+
+char gpsData[100];
+
+struct Data {
+  double Lat = 0;
+  double Long = 0;
+  double Alt = 0;
+  double Time = 0;
+} Loc;
+
+void getData() {
+  //Serial.println(F("GPS Signal: (lat, lng, alt)"));
+  Loc.Lat = gps.location.lat();
+  Loc.Long = gps.location.lng();
+  Loc.Alt = gps.altitude.meters();
+}
+
+void sendData(String dataToSend) {
+  client.println(dataToSend);
+  Serial.println("Data sent: " + dataToSend);
+}
+
+void connectToServer() {
+  client.connect(serverIP, serverPort);
+  if (client.connected()) {
+    Serial.println("Connected to the server.");
+  } else {
+    Serial.println("Connection to the server failed.");
+  }
+}
+
+
+/*
+String dataToSend() {
+  const String positionObject = " per pik";  //TODO: DENNE LINJE SKAL ERSTATTES MED LONG LAT ALT FRA GPS SIGNAL
+  return positionObject;
+}
+*/
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi.");
+  connectToServer();
+
+  ss.begin(GPS_BAUDRATE, SWSERIAL_8N1, RX2, TX2);
+  Serial.println("GPS module activating...");
+}
+
+void loop() {
+  if (ss.available() > 0) {
+    if (gps.encode(ss.read())) {
+      if (gps.location.isValid()) {
+        getData();
+        sprintf(gpsData, "%.8f,%.8f,%.2f", Loc.Lat, Loc.Long, Loc.Alt);
+        Serial.println(gpsData);
+
+        if (millis() > 5000 && gps.charsProcessed() < 10) {
+          Serial.println(F("No GPS data received: check wiring"));
+        }
+
+        if (client.connected()) {
+          sendData(gpsData);
+          delay(PCK_INTERVAL);
+        } else {
+          Serial.println("Connection lost. Reconnecting...");
+          connectToServer();
+          delay(5000);  // Retry every 5 seconds
+        }
+      }
+    }
+  }
+}
