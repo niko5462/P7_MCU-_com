1deb2bf06dbdea4f7c6a9988f7b7ff6d65cb7cd2
vlemon97 <145358653+vlemon97@users.noreply.github.com>
2023-10-23 10:20:11 +0200

    Add files via upload

---
+++ AP_for_followDrone/AP_for_followDrone.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+
+const char* apSSID = "followDrone";
+const char* apPassword = "AndersOst";
+
+WiFiServer server(80);
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.softAP(apSSID, apPassword);
+
+  IPAddress apIP(192, 168, 1, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.softAPConfig(apIP, apIP, subnet);
+
+  server.begin();
+
+  Serial.print("Access Point IP address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void loop() {
+  if (client.connected()) {
+    if (client.available()) {
+
+      String data = client.readStringUntil('\n');
+      Serial.print("Received data: ");
+      Serial.println(data);
+
+      // Process the received data as needed
+    //  client.println("Data received successfully.");
+    }
+  } else {
+    Serial.println("Client disconnected.");
+    delay(1000);
+
+    // Attempt to accept a new client connection
+    client = server.available();
+  }
+}
