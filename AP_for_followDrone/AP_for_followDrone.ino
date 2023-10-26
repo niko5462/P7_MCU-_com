3b00499e5676ae413556dd244867648640a01aca
Anders Munch Riisgaard <ariisg20@student.aau.dk>
2023-10-26 09:17:10 +0200

    added commented out code that should encrypt msg to mavlink protocol

--- AP_for_followDrone/AP_for_followDrone.ino
+++ AP_for_followDrone/AP_for_followDrone.ino
@@ -1,13 +1,16 @@
 #include <WiFi.h>
+#include <mavlink.h>
 
 const char* apSSID = "followDrone";
 const char* apPassword = "AndersOst";
 
 WiFiServer server(80);
 WiFiClient client;
+HardwareSerial cubeOrange(2);
 
 void setup() {
   Serial.begin(115200);
+  cubeOrange.begin(57600);
   WiFi.softAP(apSSID, apPassword);
 
   IPAddress apIP(192, 168, 1, 1);
@@ -24,6 +27,12 @@ void loop() {
     if (client.available()) {
       String data = client.readStringUntil('\n');
       Serial.println(data);
+      cubeOrange.println(data + "per pik");      
+     // mavlink_message_t msg;
+     // uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+     // uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+     // cubeOrange.write(buf, len);
+      delay(1000);
     }
   } else {
     Serial.println("Client disconnected.");
