cb88a12affb94e581dbfc866c73ad0844b19d9a8
Nikolaj Juel Andersen <njan20@student.aau.dk>
2023-11-28 11:18:09 +0100

    changes

--- Test code/FER_Test_code_position_/FER_Test_code_position_.ino
+++ Test code/FER_Test_code_position_/FER_Test_code_position_.ino
@@ -4,6 +4,7 @@
 #define RX2 16
 #define TX2 17
 #define PCK_INTERVAL 200
+#define PCK_VOLUME 2000
 
 const char* ssid = "followDrone";    // Same as the access point SSID
 const char* password = "AndersOst";  // Same as the access point password
@@ -11,17 +12,35 @@ const char* password = "AndersOst";  // Same as the access point password
 const char* serverIP = "192.168.1.1";  // IP address of the receiver ESP32
 const int serverPort = 80;
 
+double cnt = 1;
+
 WiFiClient client;
 
 char dataSend[100];
  
-void sendDataLoop(char array, int dataVolume) {
+void sendDataLoop(char *array, int dataVolume) {
+  double f1 = (57000+cnt)/1000;
+  double f2 = (9000+cnt)/1000;
+  sprintf(array, "%.3f,%.3f,%.2f", f1, f2, double(millis()));
+  client.println(array);                      // sends the data to the client
+  Serial.println("Data sent: " + String(array));
+  array[0] = '\0';
+  cnt += 1;
+  if (cnt >= PCK_VOLUME){
+    Serial.println("Client Disconnected");
+    client.stop();
+  }
+
+    /*
     for (int i = 0; i < dataVolume; i++){
-        sprintf(array, "%.8f,%.8f,%.2f", 57000+i, 9000+i, i);
+        float f1 = (57000+i)/1000;
+        float f2 = (9000+i)/1000;
+        sprintf(array, "%.3f,%.3f,%.2f", f1, f2, i);
         client.println(array);                      // sends the data to the client
-        Serial.println("Data sent: " + array);
+        Serial.println("Data sent: " + String(array));
         array[0] = '\0';                            // emptying the char array  
     }
+    */
 }
 
 void connectToServer() {
@@ -48,7 +67,7 @@ void setup() {
 
 void loop() {
     if (client.connected()) {
-        sendDataLoop(dataSend,int 1000);
+        sendDataLoop(dataSend, PCK_VOLUME);
         delay(PCK_INTERVAL);
     } else {
         Serial.println("Connection lost. Reconnecting...");
